d55c8d75f3fefb797356892abbbbd75f3479d336
Francisross Soares de Oliveira <franciscpd@gmail.com>
2019-06-01 10:33:04 -0300

    presentation finally

--- src/presentation.js
+++ src/presentation.js
@@ -1,35 +1,37 @@
 // Import React
-import React from 'react';
+import React from "react";
+import LogoReact from "./assets/images/logo_react.svg";
 
 // Import Spectacle Core tags
 import {
   BlockQuote,
   Cite,
   Deck,
-  Heading,
-  ListItem,
   List,
+  ListItem,
+  Heading,
   Quote,
   Slide,
   Text,
-} from 'spectacle';
+  CodePane
+} from "spectacle";
 
 // Import theme
-import createTheme from 'spectacle/lib/themes/default';
+import createTheme from "spectacle/lib/themes/default";
 
 // Require CSS
-require('normalize.css');
+require("normalize.css");
 
 const theme = createTheme(
   {
-    primary: 'white',
-    secondary: '#1F2022',
-    tertiary: '#03A9FC',
-    quaternary: '#CECECE',
+    primary: "white",
+    secondary: "#1F2022",
+    tertiary: "#03A9FC",
+    quaternary: "#CECECE"
   },
   {
-    primary: 'Montserrat',
-    secondary: 'Helvetica',
+    primary: "Montserrat",
+    secondary: "Helvetica"
   }
 );
 
@@ -37,56 +39,254 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Deck
-        transition={['zoom', 'slide']}
+        transition={["zoom", "slide"]}
         transitionDuration={500}
         theme={theme}
       >
-        <Slide transition={['zoom']} bgColor="primary">
+        <Slide transition={["zoom"]} bgColor="primary">
+          <img src={LogoReact} alt="Logo React" width="128" />
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+            React
           </Heading>
           <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+            Hooks o que são e como utilizar
           </Text>
         </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
-          </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
-        </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+        <Slide transition={["fade"]} bgColor="primary">
           <Heading size={6} textColor="secondary" caps>
-            Standard List
+            Quem sou
+          </Heading>
+          <Text size={6} textColor="tertiary">
+            Um breve resumo
+          </Text>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <img src={LogoReact} alt="Logo React" width="468" />
+          <Heading size={6} textColor="secondary" caps>
+            React? O que é?
+          </Heading>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary">
+          <Text size={6} textColor="primary">
+            React é "uma biblioteca JavaScript declarativa, eficiente e flexível
+            para a criação de interfaces de usuário (UI)"
+          </Text>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary">
+          <Heading size={6} textColor="secondary" caps>
+            Statefull
+          </Heading>
+          <CodePane
+            lang="jsx"
+            source={require("./assets/statefull.example")}
+            margin="20px auto"
+            overflow="overflow"
+          />
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary">
+          <Heading size={6} textColor="secondary" caps>
+            Stateless
+          </Heading>
+          <CodePane
+            lang="jsx"
+            source={require("./assets/stateless.example")}
+            margin="20px auto"
+            overflow="overflow"
+          />
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="secondary">
+          <Heading size={6} textColor="secondary" caps>
+            O que é Hooks?
+          </Heading>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary">
+          <Text size={6} textColor="primary">
+            É uma funcionalidade implementada a partir da versão 16.7.0 do React
+            que visa oferecer formas de trabalharmos com estado e outras API's
+            sem a necessidade de ter uma classe (stateful component).
+          </Text>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={6} textColor="secondary" caps>
+            Motivações
+          </Heading>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={6} textColor="secondary" caps>
+            HOC’s (Higher-order components)
+          </Heading>
+          <CodePane
+            lang="jsx"
+            source={require("./assets/hoc.example")}
+            margin="20px auto"
+            overflow="overflow"
+          />
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={6} textColor="secondary" caps>
+            Render props
+          </Heading>
+          <Text size={6} textColor="tertiary">
+            Compartilhamento de funcionalidade e estado do react, deixa o código
+            mais verboso.
+          </Text>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <CodePane
+            lang="jsx"
+            source={require("./assets/counterWrapper.example")}
+            margin="20px auto"
+            overflow="overflow"
+          />
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <CodePane
+            lang="jsx"
+            source={require("./assets/counterUI.example")}
+            margin="20px auto"
+            overflow="overflow"
+          />
+          <CodePane
+            lang="jsx"
+            source={require("./assets/renderProps.example")}
+            margin="20px auto"
+            overflow="overflow"
+          />
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={6} textColor="secondary" caps>
+            Finalmente Hooks
+          </Heading>
+          <img src={require("./assets/images/meme.jpeg")} alt="Enfim hooks" />
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={6} textColor="secondary" caps>
+            Hooks básicos
+          </Heading>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary">
+          <Heading size={6} textColor="primary" textAlign="left">
+            useState
+          </Heading>
+          <Text size={6} textColor="primary" textAlign="left">
+            Substitui a declaração de state = {} e o this.setState({}), retorna
+            vetor [value, fn] facilitando a desestruração para utilização.
+          </Text>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={6} textColor="secondary" caps>
+            Exemplos
+          </Heading>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <CodePane
+            lang="jsx"
+            source={require("./assets/state.example")}
+            margin="20px auto"
+            overflow="overflow"
+          />
+          <CodePane
+            lang="jsx"
+            source={require("./assets/usestate.example")}
+            margin="20px auto"
+            overflow="overflow"
+          />
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary">
+          <Text size={6} textColor="primary" textAlign="left">
+            Então para mim ter mais de um estado eu sempre tenho que declarar o
+            const [] = useState ?
+          </Text>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <CodePane
+            lang="jsx"
+            source={require("./assets/usereducer.example")}
+            margin="20px auto"
+            overflow="overflow"
+          />
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary">
+          <Heading size={6} textColor="primary" textAlign="left">
+            useEffect
+          </Heading>
+          <Text size={6} textColor="primary" textAlign="left">
+            Permite que seu componente em forma de função tenha acesso aos
+            métodos de ciclo de vida sem precisar refatorar seu componente para
+            uma classe.
+          </Text>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Text size={6} textColor="secondary" caps>
+            Lifecicles
+          </Text>
+          <CodePane
+            lang="jsx"
+            source={require("./assets/lifecicle.example")}
+            margin="20px auto"
+            overflow="overflow"
+          />
+          <CodePane
+            lang="jsx"
+            source={require("./assets/lifecicleeffects.example")}
+            margin="20px auto"
+            overflow="overflow"
+          />
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary">
+          <Heading size={6} textColor="secondary" caps>
+            usecontext
+          </Heading>
+          <Text size={6} textAlign="left" textSize={24} textColor="tertiary">
+            Para utilizarmos a Context API do React até então precisávamos
+            utilizar o pattern de render props criando um código como o
+            seguinte:
+          </Text>
+          <CodePane
+            lang="jsx"
+            source={require("./assets/contextapi.example")}
+            margin="20px auto"
+            overflow="overflow"
+          />
+          <Text size={6} textAlign="left" textSize={24} textColor="tertiary">
+            Com os hooks podemos simplesmente fazer o seguinte:
+          </Text>
+          <CodePane
+            lang="jsx"
+            source={require("./assets/usecontext.example")}
+            margin="20px auto"
+            overflow="overflow"
+          />
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary">
+          <Heading size={6} textColor="secondary" caps textAlign="left">
+            Hooks adicionais
           </Heading>
           <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+            <ListItem>useReducer</ListItem>
+            <ListItem>useCallback</ListItem>
+            <ListItem>useMemo</ListItem>
+            <ListItem>useRef</ListItem>
+            <ListItem>useImperativeHandle</ListItem>
+            <ListItem>useLayoutEffect</ListItem>
+            <ListItem>useDebugValue</ListItem>
           </List>
         </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
+        <Slide transition={["fade"]} bgColor="primary">
+          <Heading size={6} textColor="secondary" caps textAlign="left">
+            Hooks da comunidade
+          </Heading>
+          <List>
+            <ListItem>useDispatch</ListItem>
+            <ListItem>useSelector</ListItem>
+            <ListItem>useAxios</ListItem>
+            <ListItem>useClipboard</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
           <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
+            <Quote>Obrigado</Quote>
+            <Cite>Francis Soares de Oliveira</Cite>
           </BlockQuote>
         </Slide>
       </Deck>
